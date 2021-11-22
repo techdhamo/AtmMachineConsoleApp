716d395db01383ba0b2b573dd702501661ba9a3f
Administrator <Administrator@WIN-LAOP9A6C0UJ>
2021-11-22 20:56:05 +0530

    moving to testing

--- src/AtmMachine.java
+++ src/AtmMachine.java
@@ -3,6 +3,6 @@ public class AtmMachine {
 
 	public static void main(String[] args) {
 		
-		System.out.println("The machine is running :");
+		System.out.println("This is Automated Cash Deposit and Withdraw Machine version 5.0 \nStarting....\n Machine running at port 8080");
 	}
 }
